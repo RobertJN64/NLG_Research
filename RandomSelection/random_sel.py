34eb7f9f58687296fa33f8e96dcda40621f856e6
RobertJN64 <robertjnies@gmail.com>
2023-01-10 12:23:31 -0500

    don't double select emails

--- RandomSelection/random_sel.py
+++ RandomSelection/random_sel.py
@@ -3,9 +3,13 @@ import random
 with open("teachers.txt") as f:
     lines = [line.strip() for line in f.readlines()]
 
+with open("sel.txt") as f:
+    ban_list = [line.strip() for line in f.readlines()]
+
 n = int(input("Enter selection number: "))
 email_list = []
-for i in range(0, n):
+i = 0
+while i < n:
     l = random.choice(lines)
     lines.remove(l)
 
@@ -14,9 +18,20 @@ for i in range(0, n):
     email = l[2].lower()
     dept = ' '.join(l[3:])
 
+    if email in ban_list:
+        continue
+
+    if '@ccs.k12.in.us' not in email:
+        print("Error:", email)
+
     print(name + " " + dept + " (" + email + ")")
     email_list.append(email)
+    i += 1
 
 print()
 print("Emails: ")
 print(" ".join(email_list))
+
+if input("Add to sel? y/n ") == 'y':
+    with open("sel.txt", 'a') as f:
+        f.write("\n".join(email_list))
